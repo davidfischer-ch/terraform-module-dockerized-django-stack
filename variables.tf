0da633e7c8f2375baeb48175aed691253c115e29
David Fischer <david@fisch3r.net>
2023-06-16 22:36:27 +0200

    Drop site_name

--- variables.tf
+++ variables.tf
@@ -42,10 +42,6 @@ variable "project_name" {
   description = "Django project's name (directory), for example DietApp."
 }
 
-variable "site_name" {
-  type = string
-}
-
 variable "settings" {
   type        = map(string)
   default     = {}
